86fc30589cb584f9b283f60869cea0e5d6d6fdf4
Luke McDonald <lukemcdonald@MSP42TLM091022.local>
2019-10-28 11:54:58 -0400

    Updates

---
+++ precache-manifest.304922d76475403063c1e538fa2a2bf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "692a8c3ff192756dea6ba1a3d5b279be",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9e7da7bb1f2950d4f0f8",
+    "url": "/monsters-rolodex/static/css/main.f48c9edb.chunk.css"
+  },
+  {
+    "revision": "331a6c9f26a1f158523b",
+    "url": "/monsters-rolodex/static/js/2.f641515a.chunk.js"
+  },
+  {
+    "revision": "9e7da7bb1f2950d4f0f8",
+    "url": "/monsters-rolodex/static/js/main.f13d63bc.chunk.js"
+  },
+  {
+    "revision": "cf4959d4a3aa9299c4ea",
+    "url": "/monsters-rolodex/static/js/runtime-main.92f2d94f.js"
+  }
+]);
